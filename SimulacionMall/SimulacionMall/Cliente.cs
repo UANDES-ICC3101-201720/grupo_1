b875e77492f531aff849fef630befb7f3a2eebad
JuanmaCampos <30904071+JuanmaCampos@users.noreply.github.com>
2017-10-07 12:24:38 -0300

    Empece el metodo visitar tienda

--- SimulacionMall/SimulacionMall/Cliente.cs
+++ SimulacionMall/SimulacionMall/Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,10 @@ namespace SimulacionMall
         string nombreCliente;
         string rutCliente;
         int dinero;
+        int piso;
         string formaDeLlegada;
+        List<Negocio> plandeCompra;
+        List<Negocio> visitados;
 
         public Cliente(string nombreCliente, string rutCliente, int dinero, string formaDeLlegada)
         {
@@ -19,8 +22,24 @@ namespace SimulacionMall
             this.rutCliente = rutCliente;
             this.dinero = dinero;
             this.formaDeLlegada = formaDeLlegada;
+            this.piso = piso;
             List<Negocio> planDeCompra = new List<Negocio>();
             List<Negocio> visitados = new List<Negocio>();
         } 
+        public void visitarTiendas(Cliente c)
+        {
+            foreach(Negocio negocio in c.plandeCompra)
+            {
+                if (c.visitados.Contains(negocio) = false)
+                {
+                    c.visitados.Add(negocio);
+                    
+
+                }
+            }
+        }
+
+        
+            
     }
 }
