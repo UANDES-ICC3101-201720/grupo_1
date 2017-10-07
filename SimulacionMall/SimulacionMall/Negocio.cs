72f9c67ccf697d32421a6f8fe4ef8a312a6338ae
JaimeContardo <jcontardo@miuandes.cl>
2017-10-07 17:17:10 -0300

    Update Negocio.cs
    Se agregan los atributos de las categorías.

--- SimulacionMall/SimulacionMall/Negocio.cs
+++ SimulacionMall/SimulacionMall/Negocio.cs
@@ -7,23 +7,24 @@ using System.Threading.Tasks;
 namespace SimulacionMall
 {
     public class Negocio
-    {
-        string nombreNegocio;
-        int numeroClientes;
-        int piso;
-        int precioArriendo;
-        int ganancias;
-        int horaApertura;
-        int horaCierre;
-        int stock;
-        int precioMax;
-        int precioMin;
-        int areaNegocio;
-        int numEmpleados;
-        int clientesDiaAnterior;
-        int clientesDelDia;
+    { public string nombreNegocio;
+        public int numeroClientes;
+        public int piso;
+        public int precioArriendo;
+        public int ganancias;
+        public int horaApertura;
+        public int horaCierre;
+        public int stock;
+        public int precioMax;
+        public int precioMin;
+        public int areaNegocio;
+        public int numEmpleados;
+        public int clientesDiaAnterior;
+        public int clientesDelDia;
+        public string cat;
+        public string subcat;
 
-        public Negocio(string nombreNegocio, int areaNegocio, int piso, int precioArriendo, int precioMin, int precioMax, int stock, int numEmpleados, int clientesDiaAnterior, int clientesDelDia)
+        public Negocio(string nombreNegocio, int areaNegocio, int piso, int precioArriendo, int precioMin, int precioMax, int stock, int numEmpleados, int clientesDiaAnterior, int clientesDelDia, string cat, string subcat)
         {
             this.nombreNegocio = nombreNegocio;
             this.areaNegocio = areaNegocio;
@@ -35,6 +36,8 @@ namespace SimulacionMall
             this.numEmpleados = numEmpleados;
             this.clientesDiaAnterior = clientesDiaAnterior;
             this.clientesDelDia = clientesDelDia;
+            this.cat = cat;
+            this.subcat = subcat;
         }
 
 
@@ -42,8 +45,10 @@ namespace SimulacionMall
         {
             int totalNegocio = p.numLocales;
             int areaTotal = 0;
-            while (areaTotal <= p.areaPiso)
+            while (areaTotal < p.areaPiso)
             {
+                string categoria = "";
+                string subcategoria = "";
                 Console.WriteLine("Ingrese el nombre del Local o Negocio: ");
                 string nombreNegocio = Console.ReadLine();
                 Console.WriteLine("Ingrese el area del Local o Negocio: ");
@@ -58,11 +63,46 @@ namespace SimulacionMall
                 int stock = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Ingrese el numero de Empleados del Local o Negocio");
                 int numEmpleados = Convert.ToInt32(Console.ReadLine());
+
                 int clientesDiaAnterior = 0;
                 int clientesDelDia = 0;
                 if (areaTotal + areaNegocio <= p.areaPiso)
                 {
-                    Negocio n = new Negocio(nombreNegocio, areaNegocio, p.numeroPiso, precioArriendo, precioMin, precioMax, stock, numEmpleados, clientesDiaAnterior, clientesDelDia);
+                    Negocio n = new Negocio(nombreNegocio, areaNegocio, p.numeroPiso, precioArriendo, precioMin, precioMax, stock, numEmpleados, clientesDiaAnterior, clientesDelDia, categoria, subcategoria);
+                    Console.WriteLine("Ingrese la Categoria del Local o Negocio");
+                    Console.WriteLine("Ingrese 1  Comercial, 2 para Comida y 3 para Entretencion");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    if (y == 1)
+                    {
+                        Console.WriteLine("Ingrese la Subcategoria del Local o Negocio");
+                        Console.WriteLine("Ingrese 1 para Ropa, 2 para Hogar, 3 para Alimento, 4 para Ferreteria o 5 para Tecnologia");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        if (x == 1) { n.subcat = "Ropa"; }
+                        else if (x == 2) { n.subcat = "Hogar"; }
+                        else if (x == 3) { n.subcat = "Alimento"; }
+                        else if (x == 4) { n.subcat = "Ferreteria"; }
+                        else if (x == 5) { n.subcat = "Tecnologia"; }
+                        n.cat = "Comercial";
+                    }
+                    else if (y == 2)
+                    {
+                        Console.WriteLine("Ingrese la Subcategoria del Local o Negocio");
+                        Console.WriteLine("Ingrese 1 para Rapida o 2 para Restaurant");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        if (x == 1) { n.subcat = "Rapida"; }
+                        else if (x == 2) { n.subcat = "Restaurant"; }
+                        n.cat = "Comida";
+                    }
+                    else if (y == 3)
+                    {
+                        Console.WriteLine("Ingrese la Subcategoria del Local o Negocio");
+                        Console.WriteLine("Ingrese 1 para Cine, 2 para Juegos o 3 para Bowling");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        if (x == 1) { n.subcat = "Cinea"; }
+                        else if (x == 2) { n.subcat = "Juegost"; }
+                        else if (x == 3) { n.subcat = "Bowling"; }
+                        n.cat = "Juego";
+                    }
                     negocios.Add(n);
                     areaTotal += areaNegocio;
                 }
